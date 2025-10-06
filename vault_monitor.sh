1fdbcb1c5d6c54cdb913992be642e4c4fa7da0a5
nyves01 <n.yves@alustudent.com>
2025-10-06 05:28:10 -0700

    step 4

--- vault_monitor.sh
+++ vault_monitor.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-cd
-cd home
-mkdir secure_vault
-cd secure_vault
 
-touch keys.txt
-touch secrets.txt
-touch logs.txt
+# Check if vault exists
+if [ ! -d "$secure_vault" ]; then
+  echo "❌ Error: $secure_vault does not exist. Run vault_setup.sh first."
+  exit 1
+fi
 
-echo 'Welcome text' > keys.txt
-echo 'Welcome text' > secrets.txt
-echo 'Welcome Text' > logs.txt
+start report=~/secure_vault/vault_report.txt
+echo "Vault Security Report - $(date)" > $REPORT
+echo "==============================" >> $REPORT
 
-echo 'Vault Created!'
+for file in ~/secure_vault/*; do
+	name=$(basename "$file")
+	size=$(stat -c%s "$file")
+	modifiedDate=$(stat -c%y "$file")
+	permissions=$(stat -c%A "$file")
 
-ls -l
+	echo "Filename: $name" >> $report
+	echo "Size: $size bytes" >> $REPORT
+	echo "Modified date: $modifiedDate" >> $report
+	echo "Permissions: $permissions" >> $report
+
+	#check for risky permissions
+	numericPermissions=$(stat -c%a "$file")
+	if [[ $numericPermissions -gt 644 ]]; then
+		echo "⚠️ SECURITY RISK DETECTED" >> $report
+	fi
+	echo "---------------------" >> $report
+done
+echo "The vault report has been created at $report"
